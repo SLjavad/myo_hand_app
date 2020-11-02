dc4f4fd94e2556cd31b5d37f3d0404f614039a32
Javad <jd.mn98@yahoo.com>
2020-11-03 00:35:39 +0330

    Add project files.

---
+++ MyoApp/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace MyoApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "No Myo";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(17, 121);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(255, 356);
+            this.listBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(12, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "MYO Poses";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label3.Location = new System.Drawing.Point(12, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "No TCP";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 489);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Myo Pose Monitor";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MyoApp/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MyoSharp.Device;
+using MyoSharp.Communication;
+using MyoSharp.Exceptions;
+using MyoSharp.Poses;
+using System.Net.Sockets;
+using System.Net;
+
+namespace MyoApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        TcpClient tcpClient = new TcpClient();
+        NetworkStream ns;
+
+        IChannel myoChannel;
+        IHub myoHub;
+        IMyo myo;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            myoChannel = Channel.Create(ChannelDriver.Create(ChannelBridge.Create(), MyoErrorHandlerDriver.Create(MyoErrorHandlerBridge.Create())));
+            myoHub = Hub.Create(myoChannel);
+
+            myoHub.MyoConnected += MyoHub_MyoConnected;
+            myoHub.MyoDisconnected += MyoHub_MyoDisconnected;
+
+            myoChannel.StartListening();
+
+            Task.Run(() =>
+            {
+                try
+                {
+                    tcpClient.Connect(IPAddress.Parse("192.168.137.144"), 1234);
+                    tcpClient.NoDelay = true;
+                    this.Invoke(new Action(() =>
+                    {
+                        label3.Text = "TCP Connected";
+                        label3.ForeColor = Color.Green;
+                    }));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.GetBaseException().ToString());
+                    this.Invoke(new Action(() =>
+                    {
+                        label3.Text = "TCP failed to connect";
+                        label3.ForeColor = Color.Red;
+                    }));
+                }
+                
+            });
+        }
+
+        private void MyoHub_MyoDisconnected(object sender, MyoEventArgs e)
+        {
+            MessageBox.Show("disconnected");
+        }
+
+        private void MyoHub_MyoConnected(object sender, MyoEventArgs e)
+        {
+            this.Invoke(new Action(() => {
+                label1.Text = "Myo Connected";
+                label1.ForeColor = Color.Green;
+            }));
+            e.Myo.Vibrate(VibrationType.Medium);
+            myo = e.Myo;
+            e.Myo.Unlock(UnlockType.Hold);
+            var pose = HeldPose.Create(e.Myo, Pose.DoubleTap, Pose.FingersSpread, Pose.Fist, Pose.Rest, Pose.WaveIn, Pose.WaveOut);
+            pose.Interval = TimeSpan.FromSeconds(0.5);
+            pose.Start();
+            pose.Triggered += Pose_Triggered;
+        }
+
+        private void Pose_Triggered(object sender, PoseEventArgs e)
+        {
+            Console.WriteLine(e.Pose.ToString());
+
+            this.Invoke(new Action(() =>
+            {
+                try
+                {
+                listBox1.Items.Add(e.Pose);
+                listBox1.TopIndex = listBox1.Items.Count - 1;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("dakhel invoke \n"+ ex.GetBaseException().ToString());
+                }
+            }));
+            try
+            {
+                byte[] buff;
+                ns = tcpClient.GetStream();
+                buff = Encoding.ASCII.GetBytes(e.Pose.ToString()+"\r\n");
+                ns.Write(buff, 0, buff.Length);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("kharej invoke \n" + ex.GetBaseException().ToString());
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            myo.Lock();
+            myoChannel.StopListening();
+            myoHub.Dispose();
+        }
+    }
+}
